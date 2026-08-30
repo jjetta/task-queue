77339c1033f53250bfdb91ee37ccc8ad9b4c699f
JJ Etta <johnjoshetta@gmail.com>
2026-08-29 21:45:38 -0500

    chore: decrease max retries from 5 to 3
    5 seems like a bit much...

--- src/main/java/com/jjetta/task_queue/config/RetryProperties.java
+++ src/main/java/com/jjetta/task_queue/config/RetryProperties.java
@@ -7,7 +7,7 @@ import java.time.Duration;
 
 @ConfigurationProperties(prefix = "app.retry")
 public record RetryProperties(
-        @DefaultValue("5")  int maxRetries,
+        @DefaultValue("3")  int maxRetries,
         @DefaultValue("2s")  Duration baseDelay,
         @DefaultValue("60s") Duration maxDelay,
         @DefaultValue("3s")  Duration jitter

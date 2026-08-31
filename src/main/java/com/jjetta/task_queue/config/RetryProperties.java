87813ad94c8860d496135445e88a8bcd5d1b17f5
JJ Etta <johnjoshetta@gmail.com>
2026-08-31 13:00:59 -0500

    chore: update application.yaml and RetryProperties

--- src/main/java/com/jjetta/task_queue/config/RetryProperties.java
+++ src/main/java/com/jjetta/task_queue/config/RetryProperties.java
@@ -1,14 +1,13 @@
 package com.jjetta.task_queue.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.bind.DefaultValue;
 
 import java.time.Duration;
 
 @ConfigurationProperties(prefix = "app.retry")
 public record RetryProperties(
-        @DefaultValue("3")  int maxRetries,
-        @DefaultValue("2s")  Duration baseDelay,
-        @DefaultValue("60s") Duration maxDelay,
-        @DefaultValue("3s")  Duration jitter
+        int maxRetries,
+        Duration baseDelay,
+        Duration maxDelay,
+        Duration jitter
 ) {}

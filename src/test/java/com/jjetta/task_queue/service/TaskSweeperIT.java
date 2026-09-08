e12063f03cccad46f114ecd5e8ea6fe2387343fd
JJ Etta <johnjoshetta@gmail.com>
2026-09-08 15:27:34 -0500

    test: Add integration test for eviction sweeper

--- src/test/java/com/jjetta/task_queue/service/TaskSweeperIT.java
+++ src/test/java/com/jjetta/task_queue/service/TaskSweeperIT.java
@@ -44,12 +44,12 @@ public class TaskSweeperIT {
     public void setup() {
         taskRepository.deleteAll();
 
-        Task task = Task.createTask("stale-job", Map.of("arg1", "param1"));
-        ReflectionTestUtils.setField(task, "status", TaskStatus.RUNNING);
-        ReflectionTestUtils.setField(task, "claimedAt", Instant.now().minusSeconds(30));
-        ReflectionTestUtils.setField(task, "claimToken", UUID.randomUUID());
+        Task staleTask = Task.createTask("stale-job", Map.of("arg1", "param1"));
+        ReflectionTestUtils.setField(staleTask, "status", TaskStatus.RUNNING);
+        ReflectionTestUtils.setField(staleTask, "claimedAt", Instant.now().minusSeconds(30));
+        ReflectionTestUtils.setField(staleTask, "claimToken", UUID.randomUUID());
 
-        taskRepository.save(task);
+        taskRepository.save(staleTask);
     }
 
     @Test
@@ -82,9 +82,64 @@ public class TaskSweeperIT {
                 .build();
 
         Optional<Task> taskB = taskRepository.findById(taskId);
+        assertThat(taskB.isPresent()).isTrue();
         taskService.reportTaskOutcome(taskId, taskReport);
 
         assertThatThrownBy(() -> taskSweeperService.timeoutStaleTask(taskB.get()))
                 .isInstanceOf(ObjectOptimisticLockingFailureException.class);
+    }
+
+    @Test
+    public void shouldEvictTaskSuccessfully() {
+        // Tasks eligible for eviction meet the following criteria:
+        // - they have a PENDING status
+        // - they have a failureCount of ZERO
+        // - they are at least one minute old
+
+        // This task is PENDING, has ZERO failures, and is outside the creation window
+        // Therefore, it should get evicted
+        Task taskA =  Task.createTask("unclaimed-job", Map.of("arg1", "param1"));
+        ReflectionTestUtils.setField(taskA, "createdAt", Instant.now().minusSeconds(61));
+        Long taskAId = taskRepository.save(taskA).getId();
+
+        // This task is PENDING and is outside the creation window, but has failed once
+        // Therefore, it should NOT get evicted
+        Task taskB =  Task.createTask("unclaimed-job", Map.of("arg1", "param1"));
+        ReflectionTestUtils.setField(taskB, "createdAt", Instant.now().minusSeconds(61));
+        ReflectionTestUtils.setField(taskB, "failureCount", 1);
+        Long taskBId = taskRepository.save(taskB).getId();
+
+        // This task may be outside the creation window, but is RUNNING
+        // Therefore, it should NOT get evicted
+        Task taskC = Task.createTask("unclaimed-job", Map.of("arg1", "param1"));
+        ReflectionTestUtils.setField(taskC, "createdAt", Instant.now().minusSeconds(61));
+        ReflectionTestUtils.setField(taskC, "status", TaskStatus.RUNNING);
+        Long taskCId = taskRepository.save(taskC).getId();
+
+
+        // This task is within the creation window
+        // Therefore, it should NOT get evicted
+        Task taskD = Task.createTask("unclaimed-job", Map.of("arg1", "param1"));
+        ReflectionTestUtils.setField(taskD, "createdAt", Instant.now().minusSeconds(55));
+        Long taskDId = taskRepository.save(taskD).getId();
+
+        taskSweeper.evictUnclaimedTasks();
+
+        Optional<Task> resultTaskA =  taskRepository.findById(taskAId);
+        assertThat(resultTaskA.isPresent()).isTrue();
+        assertThat(resultTaskA.get().getStatus()).isEqualTo(TaskStatus.DEAD);
+        assertThat(resultTaskA.get().getFailureCount()).isZero();
+
+        Optional<Task> resultTaskB =  taskRepository.findById(taskBId);
+        assertThat(resultTaskB.isPresent()).isTrue();
+        assertThat(resultTaskB.get().getStatus()).isEqualTo(TaskStatus.PENDING);
+
+        Optional<Task> resultTaskC =  taskRepository.findById(taskCId);
+        assertThat(resultTaskC.isPresent()).isTrue();
+        assertThat(resultTaskC.get().getStatus()).isEqualTo(TaskStatus.RUNNING);
+
+        Optional<Task> resultTaskD =  taskRepository.findById(taskDId);
+        assertThat(resultTaskD.isPresent()).isTrue();
+        assertThat(resultTaskD.get().getStatus()).isEqualTo(TaskStatus.PENDING);
     }
 }

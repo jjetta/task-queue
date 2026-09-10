75ab8e5ecdd67344aba1e140c8853760e3e6df6c
JJ Etta <johnjoshetta@gmail.com>
2026-09-09 20:20:37 -0500

    dlq: Add exception handling for dead-letter endpoints

--- src/main/java/com/jjetta/task_queue/exception/TaskNotRunningException.java
+++ src/main/java/com/jjetta/task_queue/exception/TaskNotRunningException.java
@@ -5,6 +5,6 @@ import com.jjetta.task_queue.model.TaskStatus;
 public class TaskNotRunningException extends IllegalStateException {
 
     public TaskNotRunningException(Long id, TaskStatus status) {
-        super("Attempt to report execution outcome on task with id: " + id + ", but said task is " + status);
+        super("Attempt to report execution outcome on task with id: " + id + ", but its status is " + status);
     }
 }
